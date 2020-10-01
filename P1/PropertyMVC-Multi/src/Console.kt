64fea7e4bf08404e1bc3fdda1c5b41bbfd566ad2
Aniol Marti <aniol@aniolmarti.cat>
2020-10-01 19:07:49 +0200

    WIP: end of terminal and multithread

--- P1/PropertyMVC-Multi/src/Console.kt
+++ P1/PropertyMVC-Multi/src/Console.kt
@@ -3,6 +3,7 @@ import java.beans.PropertyChangeListener
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.*
+import kotlin.concurrent.thread
 
 class Console() : PropertyChangeListener {
     var position: Int = 0
@@ -10,10 +11,25 @@ class Console() : PropertyChangeListener {
     var cursorPosition = intArrayOf(0, 0)
     var consoleSize: IntArray = intArrayOf(0, 0)
     var lines: LinkedList<*> = LinkedList<Line>()
+    var runningThread = false
 
     init {
         consoleSize = updateConsoleSize()
         printLines()
+        // Start a thread to check the window size.
+        thread(start = runningThread) {
+            var prevSize: IntArray
+            while (runningThread) {
+                prevSize = consoleSize
+                consoleSize = updateConsoleSize()
+                if (!prevSize.contentEquals(consoleSize)) {
+                    cursorPosition[0] = position / consoleSize[1] + getPrintedLines(currentLine)
+                    cursorPosition[1] = position % consoleSize[1]
+                    printLines()
+                }
+                Thread.sleep(50)
+            }
+        }
     }
 
     private fun updateConsoleSize(): IntArray {
@@ -43,7 +59,11 @@ class Console() : PropertyChangeListener {
 
     private fun printLines() {
         print("\u001bc\u001b[3J\u001b[H\u001b[2J") // Reset, clean (2J) and move the cursor to (0,0) (H).
-        for (line in lines) {
+        // WIP
+        val firstLine = Integer.max((getPrintedLines(currentLine) / consoleSize[0]) * (consoleSize[0] + 1) - (getPrintedLines(currentLine) / consoleSize[0]), 0)
+        val lastLine = Integer.min(firstLine + consoleSize[0], lines.size)
+        //
+        for (line in lines.subList(firstLine, lastLine)) {
             print((line as Line).text)
         }
         print("\u001b[" + (cursorPosition[0] + 1).toString() + ";" + (cursorPosition[1] + 1).toString() + "H") // Move the cursor to position.
@@ -69,7 +89,7 @@ class Console() : PropertyChangeListener {
             else if (p0.propertyName == "lineArray") {
                 lines = p0.newValue as LinkedList<*>
             }
-            cursorPosition[0] = position / consoleSize[1] + getPrintedLines(currentLine)
+            cursorPosition[0] = (position / consoleSize[1] + getPrintedLines(currentLine)) % consoleSize[0]
             cursorPosition[1] = position % consoleSize[1]
             printLines()
         }
