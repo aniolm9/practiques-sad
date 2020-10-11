95d7efc5d4796796a7b201bd6a52fa3ab4ff2dac
Aniol Marti <aniol@aniolmarti.cat>
2020-10-11 19:31:50 +0200

    Do not use console, just Kotlin readline

--- P2/Mutex/src/client/WriteThread.kt
+++ P2/Mutex/src/client/WriteThread.kt
@@ -1,31 +1,18 @@
 package client
 
 import MySocket
-import java.lang.NullPointerException
 
 class WriteThread(private val socket: MySocket, var username: String): Thread() {
     override fun run() {
         // Send username to server.
         socket.writeMsg(username)
 
-        val console = System.console()
-        var text: String?
-        // Check if we are running inside and IDE or not.
-        text = try {
-            console.readLine("[$username] ")
-        }
-        catch (e: NullPointerException) {
-            readLine()
-        }
+        print("[you] ")
+        var text = readLine()
         while (text != null) {
             socket.writeMsg(text)
-            // Check if we are running inside and IDE or not.
-            text = try {
-                console.readLine("[$username] ")
-            }
-            catch (e: NullPointerException) {
-                readLine()
-            }
+            print("[you] ")
+            text = readLine()
         }
         socket.close()
     }
