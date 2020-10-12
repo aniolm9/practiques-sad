2a9e5b6abca90d98293aff9e46f40c12be5bb21b
Hugation99 <34241216+Hugation99@users.noreply.github.com>
2020-10-12 21:12:46 +0200

    Update MyServerSocket.kt
    MyServerSocket 1.0

--- P2/Mutex/src/MyServerSocket.kt
+++ P2/Mutex/src/MyServerSocket.kt
@@ -1,2 +1,25 @@
-class MyServerSocket {
+class MyServerSocket : ServerSocket{
+    constructor():super()
+    fun threadServer(){
+        var port = 4444;
+        try {
+            var  serverSocket:  ServerSocket = ServerSocket(port)
+        } catch (e: IOException){
+            println("port error")
+        }
+    }
+    fun acceptClients(){
+        var clients: Map<client,client.getSocket()>
+        while(true){
+            try{
+                var socket:Socket = ServerSocket
+                var client: Client() = Client(Socket)
+                var thread: Thread(client)
+                thread.start()
+                clients.put(client,client.socket)
+            } catch (e: IOException){
+                println("acceptació falla a:" )
+            }
+        }
+    }
 }
