aa813f4ad04f9324e0bceb1949368b55ec870868
Aniol Marti <aniol@aniolmarti.cat>
2020-11-22 13:46:34 +0100

    Add license header in every file

--- sadnotes/smallnote.cpp
+++ sadnotes/smallnote.cpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020, Aniol Marti & Hugo Martínez
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You should have received a copy of the License. If not,
+ * see <https://opensource.org/licenses/BSD-3-Clause>.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #include "smallnote.h"
 #include <QDebug>
 
