03fb4ac21b4daab2914e4b722cf62b63576ada92
Aniol Marti <aniol@aniolmarti.cat>
2020-11-22 15:31:11 +0100

    Add tooltips to dashboard

--- sadnotes/resources/l10n/sadnotes_ca.ts
+++ sadnotes/resources/l10n/sadnotes_ca.ts
@@ -10,21 +10,41 @@
     </message>
     <message>
         <location filename="../../ui/dashboard.ui" line="44"/>
+        <source>Add a new note</source>
+        <translation>Afegeix una nota nova</translation>
+    </message>
+    <message>
+        <location filename="../../ui/dashboard.ui" line="80"/>
+        <source>Save all notes</source>
+        <translation>Desa totes les notes</translation>
+    </message>
+    <message>
+        <location filename="../../ui/dashboard.ui" line="103"/>
+        <source>Remove current note</source>
+        <translation>Elimina la nota actual</translation>
+    </message>
+    <message>
+        <location filename="../../ui/dashboard.ui" line="126"/>
+        <source>Open current note in a new window</source>
+        <translation>Obre la nota actual en una finestra nova</translation>
+    </message>
+    <message>
+        <location filename="../../ui/dashboard.ui" line="47"/>
         <source>🞤</source>
         <translation>🞤</translation>
     </message>
     <message>
-        <location filename="../../ui/dashboard.ui" line="77"/>
+        <location filename="../../ui/dashboard.ui" line="83"/>
         <source>✓</source>
         <translation>✓</translation>
     </message>
     <message>
-        <location filename="../../ui/dashboard.ui" line="97"/>
+        <location filename="../../ui/dashboard.ui" line="106"/>
         <source>✗</source>
         <translation>✗</translation>
     </message>
     <message>
-        <location filename="../../ui/dashboard.ui" line="117"/>
+        <location filename="../../ui/dashboard.ui" line="129"/>
         <source>➤</source>
         <translation>➤</translation>
     </message>
@@ -33,7 +53,7 @@
     <name>Note</name>
     <message>
         <location filename="../../ui/note.ui" line="14"/>
-        <location filename="../../note.cpp" line="30"/>
+        <location filename="../../note.cpp" line="47"/>
         <source>Note</source>
         <translation>Nota</translation>
     </message>
@@ -48,22 +68,22 @@
         <translation>Tanca</translation>
     </message>
     <message>
-        <location filename="../../note.cpp" line="31"/>
+        <location filename="../../note.cpp" line="48"/>
         <source>The document has been modified.</source>
         <translation>El document ha estat modificat.</translation>
     </message>
     <message>
-        <location filename="../../note.cpp" line="32"/>
+        <location filename="../../note.cpp" line="49"/>
         <source>Do you want to save your changes?</source>
         <translation>Vols desar els canvis?</translation>
     </message>
     <message>
-        <location filename="../../note.cpp" line="81"/>
+        <location filename="../../note.cpp" line="98"/>
         <source>Fatal error</source>
         <translation>Error crític</translation>
     </message>
     <message>
-        <location filename="../../note.cpp" line="82"/>
+        <location filename="../../note.cpp" line="99"/>
         <source>The database is corrupted.</source>
         <translation>La base de dades està corrompuda.</translation>
     </message>
@@ -71,18 +91,18 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../main.cpp" line="42"/>
+        <location filename="../../main.cpp" line="58"/>
         <source>Operating system not supported.</source>
         <translation>Sistema operatiu no suportat.</translation>
     </message>
     <message>
-        <location filename="../../main.cpp" line="41"/>
-        <location filename="../../main.cpp" line="56"/>
+        <location filename="../../main.cpp" line="57"/>
+        <location filename="../../main.cpp" line="72"/>
         <source>Fatal error</source>
         <translation>Error crític</translation>
     </message>
     <message>
-        <location filename="../../main.cpp" line="57"/>
+        <location filename="../../main.cpp" line="73"/>
         <source>Couldn&apos;t connect to database.</source>
         <translation>No s&apos;ha pogut connectar amb la base de dades.</translation>
     </message>
